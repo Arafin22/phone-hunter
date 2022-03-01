6dbebaecdfe77968aa2e2f3077121a0a7a8288c1
arafin0 <sultanularafin000@gmail.com>
2022-03-01 22:54:16 +0600

    add single show function

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ const showAllPhone =(phones)=>{
                     <h5>Brand: ${phone.brand}</h5>
                     <h4>Model: ${phone.phone_name}</h4>
                 
-                <button class="btn btn-success">Details</button>
+                <button onclick="showOne('${phone.slug}')" class="btn btn-success">Details</button>
 
             </div>
           </div>
@@ -54,10 +54,115 @@ const showAllPhone =(phones)=>{
        }
        document.getElementById("search-box").value="";
       toggleSpinner('none');
-      
+      const showOne =document.getElementById("show-one");
+      showOne.innerHTML = "";
     }else{
         document.getElementById("search-box").value="";
         toggleSpinner('none');
-        
+        const showOne =document.getElementById("show-one");
+        showOne.innerHTML = "";
     }
+}
+
+// show one divice function 
+const showOne =slug=>{
+    //const showOne =document.getElementById("show-one");
+    const url =`https://openapi.programming-hero.com/api/phone/${slug}`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => setDetails(data.data));
+
+}
+
+
+const setDetails =info=>{
+    const showOne =document.getElementById("show-one");
+    showOne.innerHTML = "";
+    console.log(info);
+
+    //const showdetails =document.getElementById("show-details");
+    //showdetails.innerHTML = "";
+   
+
+    let sensors = info.mainFeatures.sensors;
+    let chipSet = info.mainFeatures.chipSet;
+    let displaySize = info.mainFeatures.displaySize;
+    //let bluet = info.others.Bluetooth;
+
+    console.log(info.releaseDate);
+    const error="not found";
+     //phone?.date === undefined? error:phone.date
+    
+console.log(chipSet);
+    //console.log("showOne");
+    const div = document.createElement("div");
+    div.innerHTML=`
+     
+
+    <div class="row">
+    <div class="col-sm-12 col-md-6 col-lg-6 col-12">
+        <img class="img-fluid ps-5" src="${info.image}" alt="">
+    </div>
+
+    <div class="col-sm-12 col-md-6 col-lg-6 col-12 ">
+        <div class="align-items-center">
+        <div class="pt-5">
+          <h2>${info.name}</h2>
+          <h2>${info?.releaseDate === ""? error:info.releaseDate}</h2>
+          <h2>${info?.releaseDate === undefined? error:info.releaseDate
+          }</h2>
+          
+          <p>when we see pictures ,it remain essential as they've ever been. They allow us to not only freeze the precious moments</p>
+          <button onclick="showSensor('${
+            sensors}')" type="button" class="bg-light text-primary btn  btn-lg ">Sensors</button>
+          <button onclick="showSensor('${
+            chipSet}')" type="button" class="bg-light text-primary btn  btn-lg ">chipSet</button>
+
+            <div id="show-details"></div>
+
+        </div>
+        </div>
+    </div>
+</div>
+  
+
+    `;
+    showOne.appendChild(div);
+   // console.log(div);
+
+}
+
+const showSensor = (chip) =>{
+
+  //const chips = Object.entries(chip);
+
+  console.log(chip);
+  //console.log(display);
+
+    const showdetails =document.getElementById("show-details");
+  showdetails.innerHTML =
+`<table class="table">
+
+
+<tbody>
+ 
+  <tr>
+   
+    <td>${chip}</td>
+  </tr>
+  
+</tbody>
+</table>`;
+
+
+  // const showdetails =document.getElementById("show-details");
+  // showdetails.innerHTML = "";
+  // let txt ="";
+  // for(const data in datas){
+
+  //   txt += datas[data]; 
+  // }
+  // showdetails.innerHTML=txt;
+  //console.log("showOne");
+  
 }
