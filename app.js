2988ef28cd563c024e2ec4af80b9b99cc5ee4fe1
arafin0 <sultanularafin000@gmail.com>
2022-03-01 22:17:17 +0600

    fatch all data in console

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('first');
+const allPhones =()=>{
+   
+
+    let search= document.getElementById("search-box").value;
+     const searchvalue = search.toLowerCase();
+
+     const url=`https://openapi.programming-hero.com/api/phones?search=${searchvalue}`;
+     fetch(url)
+     .then((response) => response.json())
+     .then((data) => {console.log(data.data)});
+}
