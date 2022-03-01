ec2801d384d28d1772fde34c6d5333bf94388743
arafin0 <sultanularafin000@gmail.com>
2022-03-01 23:32:31 +0600

    add single show function

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const allPhones =()=>{
     
     document.getElementById("phone-show").innerHTML = "";
+    const showOne =document.getElementById("show-one");
+    showOne.innerHTML = "";
     let search= document.getElementById("search-box").value;
      const searchvalue = search.toLowerCase();
 
@@ -86,37 +88,45 @@ const setDetails =info=>{
 
     let sensors = info.mainFeatures.sensors;
     let chipSet = info.mainFeatures.chipSet;
-    let displaySize = info.mainFeatures.displaySize;
     //let bluet = info.others.Bluetooth;
 
     console.log(info.releaseDate);
-    const error="not found";
+    const error="Not Found";
      //phone?.date === undefined? error:phone.date
     
 console.log(chipSet);
+  console.log(info);
     //console.log("showOne");
     const div = document.createElement("div");
     div.innerHTML=`
      
 
     <div class="row">
-    <div class="col-sm-12 col-md-6 col-lg-6 col-12">
-        <img class="img-fluid ps-5" src="${info.image}" alt="">
+    <div class="col-sm-12 col-md-6 col-lg-6 col-12 text-center">
+        <img class="img-fluid p-5" src="${info.image}" alt="">
+        <h6>Model No: ${info.name}</h6>
+
     </div>
 
     <div class="col-sm-12 col-md-6 col-lg-6 col-12 ">
         <div class="align-items-center">
         <div class="pt-5">
-          <h2>${info.name}</h2>
-          <h2>${info?.releaseDate === ""? error:info.releaseDate}</h2>
-          <h2>${info?.releaseDate === undefined? error:info.releaseDate
-          }</h2>
+          <h3>Specification</h3>
+          <h6>Release Date: ${info?.releaseDate === ""? error:info.releaseDate}</h6>
+          <h6>Display: ${info.mainFeatures.displaySize[0]} inches</h6>
+          <h6>Memory: ${info.mainFeatures.memory}</h6>
+         
+          <h6>GPS: ${info?.others.GPS === ""? error:info.others.GPS}</h6>
+          <h6>Radio: ${info?.others.Radio === ""? error:info.others.Radio}</h6>
+          <h6>USB: ${info?.others.USB === ""? error:info.others.USB}</h6>
+          <h6>WLAN: ${info?.others.WLAN === ""? error:info.others.WLAN}</h6>
           
-          <p>when we see pictures ,it remain essential as they've ever been. They allow us to not only freeze the precious moments</p>
+          
+          
           <button onclick="showSensor('${
-            sensors}')" type="button" class="bg-light text-primary btn  btn-lg ">Sensors</button>
+            sensors}')" type="button" class="bg-light text-primary btn  btn-lg mb-2 ">Sensors</button>
           <button onclick="showSensor('${
-            chipSet}')" type="button" class="bg-light text-primary btn  btn-lg ">chipSet</button>
+            chipSet}')" type="button" class="bg-light text-primary btn  btn-lg mb-2">chipSet</button>
 
             <div id="show-details"></div>
 
@@ -155,14 +165,6 @@ const showSensor = (chip) =>{
 </table>`;
 
 
-  // const showdetails =document.getElementById("show-details");
-  // showdetails.innerHTML = "";
-  // let txt ="";
-  // for(const data in datas){
-
-  //   txt += datas[data]; 
-  // }
-  // showdetails.innerHTML=txt;
-  //console.log("showOne");
+ 
   
 }
